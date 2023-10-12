09f73b47cc9c73f90c9624631412f6a1d3bccaf2
胡海星 <huhaixing@njzhyl.cn>
2023-10-12 15:37:48 +0800

    feat: finish the first draft

---
+++ src/getter.js
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2022 - 2023.
+ *    Haixing Hu, Qubit Co. Ltd.
+ *
+ *    All rights reserved.
+ *
+ ******************************************************************************/
+import { createDecorator } from '@haixing_hu/vue3-class-component';
+import { mapState } from 'pinia';
+
+/**
+ * Injects a Pinia store's getter to a Vue component class.
+ *
+ * This decorator should be used to decorate a class field.
+ *
+ * @param {object} store
+ *     The Pinia store to be injected, which should be the object returned by
+ *     the Pinia's `defineStore()` function..
+ * @param {string} getterName
+ *     The name of the getter to be injected. If this argument is not specified,
+ *     use the name of the decorated class field as the key.
+ * @return {function}
+ *     A class field decorator function.
+ */
+function Getter(store, getterName = undefined) {
+  return createDecorator((Class, instance, target, context, options) => {
+    if (context?.kind !== 'field') {
+      throw new Error('The @Getter decorator can only be used to decorate a class field.');
+    }
+    const key = getterName ?? context.name;
+    delete options.fields[context.name];
+    options['computed'][key] = mapState(store, key);
+  });
+}
+
+export default Getter;
